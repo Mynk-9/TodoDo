eeffca63b2cf5bcc682cf7416198d74ee321417e
Mynk-9 <Mynk-9@users.noreply.github.com>
2020-09-03 20:56:27 +0530

    Made the code more modular

--- tododo.py
+++ tododo.py
@@ -1,15 +1,42 @@
 from PIL import Image, ImageDraw, ImageFont
+import ctypes
+import pathlib
 
-image = Image.open('assets/background5.jpg')
 
+class wallpaperSetter:
+    def __init__(self, imageName):
+        self.name = imageName
+        self.path = str(pathlib.Path().absolute())
+
+    def makeWallpaper(self):
+        SPI_SETDESKWALLPAPER = 20
+        ctypes.windll.user32.SystemParametersInfoW(
+            SPI_SETDESKWALLPAPER, 0, self.path+"\\"+self.name, 0)
+
+
+class TodoGenerator:
+    def __init__(self):
+        self.todo = ""
+        padding = " "*3
+        for i in range(1, 5+1):
+            self.todo += padding + "task" + str(i) + "\n"
+
+    def getList(self):
+        return self.todo
+
+
+image = Image.open('assets/background1.jpg')
 draw = ImageDraw.Draw(image)
-
 font = ImageFont.truetype('fonts/OpenSans-SemiBoldItalic.ttf', size=55)
 
-(x,y) = (960, 50)
-message = "test message1"
-color = 'white'
+(x, y) = (960, 50)
+todo = TodoGenerator()
+message = "Todo List:\n" + todo.getList()
+color = 'black'
 
-draw.text((x,y), message, font=font, fill=color)
+draw.text((x, y), message, font=font, fill=color)
 
 image.save('output.jpg')
+
+ws = wallpaperSetter("output.jpg")
+ws.makeWallpaper()
